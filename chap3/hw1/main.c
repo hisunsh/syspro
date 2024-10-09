157810c997b6232261e248b2ca6381b16edc7cf2
seunghan <hisunsh@cu.ac.kr>
2024-10-09 07:33:49 +0000

    Final code revision completed. Please enter the commit message for your changes. Lines starting

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include "copy.h"
+
+#define MAXLINE 100
+#define MAXSTRINGS 5
 
 char line[MAXLINE];
 char longest[MAXLINE];
+char sentences[MAXSTRINGS][MAXLINE];
 
 int main() {
 	int len;
@@ -10,22 +15,30 @@ int main() {
 	max = 0;
 
 	int count = 0;
-	while(count < 5) {
-		if (gets(line) != NULL) {
+	while(count < MAXSTRINGS) {
+		if (fgets(line, MAXLINE, stdin) != NULL) {
+			line[strcspn(line, "\n")] = 0;
 			len = strlen(line);
+			strcpy(sentences[count], line);
+			count++;
 		}
-
-		if (len > max) {
-			max = len;
-			copy(line, longest);
-		}
-
-		count++;
 	}
 
-	if (max > 0) {
-		printf("%s \n", longest);
+	for (int i = 0; i < MAXSTRINGS; i++) {
+		for (int j = i + 1; j < MAXSTRINGS; j++) {
+			if (strlen(sentences[i]) < strlen(sentences[j])) {
+				char temp[MAXLINE];
+				strcpy(temp, sentences[i]);
+				strcpy(sentences[i], sentences[j]);
+				strcpy(sentences[j], temp);
+			}
+		}
 	}
+
+
+	for (int i = 0; i < MAXSTRINGS; i++) {
+			printf("%s\n", sentences[i]);
+		}
 
 	return 0;
 }
