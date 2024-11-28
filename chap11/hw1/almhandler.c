9eba025dcddc525f0d45e64735b01a2605e3bb70
seunghan <hisunsh@cu.ac.kr>
2024-11-28 02:18:03 +0000

    add chap11/hw1

---
+++ chap11/hw1/almhandler.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void alarmHandler();
+
+int main() {
+	struct sigaction sa;
+	sa.sa_handler = alarmHandler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+
+	if (sigaction(SIGALRM, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+	
+	alarm(5);
+
+	short i = 0;
+	while(1) {
+		sleep(1);
+		i++;
+		printf("%d second\n", i);
+	}
+	printf("end\n");
+
+	return 0;
+}
+
+void alarmHandler(int signo) {
+	printf("Wake up\n");
+	exit(0);
+}
